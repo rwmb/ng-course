8b15e05dcfeb54d07df5680653af6e37c3414c6d
Richard William <richard.wmbv@gmail.com>
2017-11-06 18:35:36 +0000

    fix store provider

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Store } from '@ngrx/store/src/store';
+import { Store } from '@ngrx/store';
 
 import { AuthService } from '../auth.service';
 import * as fromApp from '../../store/app.reducers';
